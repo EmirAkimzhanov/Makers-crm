96265ab1b2138ca8dd9659cc9faf10e2267c9797
Abbas <zhunushovabbas@gmail.com>
2022-10-07 10:53:22 +0600

    created tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import BuildRoutes from "./BuildRoutes";
 import Navbar from "./components/navbar/navbar";
 import { store } from "./store/store";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +12,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
+      <h1 className="text-3xl font-bold text-cyan-200 underline">
+        Hello world!
+      </h1>
+
       <BuildRoutes />
     </BrowserRouter>
   </Provider>
